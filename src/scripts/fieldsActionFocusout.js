211ec02ef48dc891da09e89ffb7b16b02e4bd476
tishinskiy <tishinskiy@mail.ru>
2020-01-23 10:49:21 +0700

    correct focusout actions

--- src/scripts/fieldsActionFocusout.js
+++ src/scripts/fieldsActionFocusout.js
@@ -1,104 +1,92 @@
 
 export default function(name){
 
+	const Store = this.store.readState()[this.name]
+	const node = this.node
+	const current = Store.readState().current
+	const changeEvent = `change${this.name[0].toUpperCase()}${this.name.slice(1)}`
+
+	const fieldAction = (func_1, func_2,) => {
+
+		if ($(node).val() === '') {
+
+			func_2()
+		} else {
+
+			this.addEmitter({
+				event: changeEvent,
+
+				action() {
+					clearTimeout(timer)
+				}
+			})
+
+			const timer = setTimeout(func_1, 300)
+		}
+	}
+
+	const func = (arr, fieldId, fieldName) => {
+
+			Store.updateState(state => ({
+			...state,
+			current: arr.length ? arr[0] : false
+		}))
+
+		if (current && arr.length && arr[0][fieldId] === current[fieldId]) {
+			$(node).val(arr[0][fieldName])
+			$(node).siblings('.ttk__droplist').remove()
+			return false
+		}
+
+		if (!current) {
+
+			if (arr.length) {
+				$(node).val(arr[0][fieldName])
+			}
+		}
+
+		this.observable.eventEmitter(changeEvent)
+	}
+
 	switch (this.name) {
 		case 'city': {
 
-			const Store = this.store.readState().city
-			const node = this.node
+			fieldAction(() => {
 
-			if ($(node).val() === '') {
+				const cities = this.store.readState().Requests.readState().cities
+
+				if (cities && cities.length) {
+
+					const arr = cities.filter(item => $(node).val().toLowerCase() === item['EXTERNAL_NAME'].toLowerCase())
+
+					func(arr, 'EXTERNAL_ID', 'EXTERNAL_NAME')
+				}
+			},
+
+			() => {
 
 				$(node).val('current' in Store.readState() ? Store.readState().current['EXTERNAL_NAME'] : '')
-			} else {
-
-				let marker = true
-
-				this.addEmitter({
-					event: 'changeCity',
-					action() {
-						marker = !marker
-					}
-				})
-
-				setTimeout(() => {
-
-					if (marker) {
-
-						const cities = this.store.readState().Requests.readState().cities
-						if (cities && cities.length) {
-
-							const arr = cities.filter(item => $(node).val().toLowerCase() === item['EXTERNAL_NAME'].toLowerCase())
-
-							if (arr.length) {
-
-								Store.updateState(state => ({
-									...state,
-									current: arr[0]
-								}))
-								this.observable.eventEmitter('changeCity')
-								
-							} else {
-
-								if (Store.readState().current) {
-									Store.updateState(state => ({
-										...state,
-										current: false
-									}))
-									this.observable.eventEmitter('changeCity')
-								}
-							}
-						}
-					}
-				}, 200)
-			}
+			})
 
 			break
 		}
 
 		case 'street': {
 
+			fieldAction(() => {
 
-			const Store = this.store.readState().street
-			const node = this.node
-			const current = Store.readState().current
+				const arr = this.dropList.list.filter(item => {
 
-			if ($(node).val() === '') {
+					return (($(node).val().toLowerCase() === item['STREET_NAME'].toLowerCase()) && (!!this.type ? this.type.toLowerCase() === item['TYPE_NAME'].toLowerCase() : true))
+				})
+
+				func(arr, 'STREET_ID', 'STREET_NAME')
+
+			}, () => {
 
 				$(node).val(current ? Store.readState().current['STREET_NAME'] : '')
 				$(node).siblings('.ttk__input__label').html(current['TYPE_NAME'])
-
-			} else {
-
-
-				let marker = true
-
-				this.addEmitter({
-					event: 'changeStreet',
-					action() {
-						marker = !marker
-					}
-				})
-
-				setTimeout(() => {
-
-					if (marker) {
-
-						const arr = this.dropList.list.filter(item => {
-
-							return (($(node).val().toLowerCase() === item['STREET_NAME'].toLowerCase()) && (!!this.type ? this.type.toLowerCase() === item['TYPE_NAME'].toLowerCase() : true))
-						})
-
-						Store.updateState(state => ({
-							...state,
-							current: arr.length ? arr[0] : false
-						}))
-
-						this.observable.eventEmitter('changeStreet')
-					}
-				}, 200)
-
-			}
+			})
 
 			break
 		}
