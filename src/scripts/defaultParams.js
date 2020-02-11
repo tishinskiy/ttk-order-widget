5c5a3b120b058062264461b26087c6d55ff8a7ab
tishinskiy <tishinskiy@mail.ru>
2020-02-11 16:14:45 +0700

    correct api path

--- src/scripts/defaultParams.js
+++ src/scripts/defaultParams.js
@@ -16,11 +16,13 @@ export const params = {
 export const strict = {
 	api: {
 		city: 'https://gate.myttk.ru/gate/jsonp/city.php',
-		street: 'https://gate.myttk.ru/gate/jsonp/street.php',
-		building: 'https://gate.myttk.ru/gate/jsonp/building.php',
+		street: 'https://gate.myttk.ru/gate/jsonp/street_new.php',
+		// street: 'https://gate.myttk.ru/gate/jsonp/street.php',
+		building: 'https://gate.myttk.ru/gate/jsonp/building_new.php',
+		// building: 'https://gate.myttk.ru/gate/jsonp/building.php',
 	},
-	// requestUrl:   'https://myttk.ru/gate/jsonp/send.php',
-	requestUrl: 'http://localhost:7000/jsonp/200',
-	// collectorUrl: 'https://myttk.ru/gate/jsonp/send.php',
-	collectorUrl: 'http://localhost:7000/collector/200',
+	requestUrl:   'https://myttk.ru/gate/jsonp/send.php',
+	// requestUrl: 'http://localhost:7000/jsonp/200',
+	collectorUrl: 'https://myttk.ru/gate/jsonp/send.php',
+	// collectorUrl: 'http://localhost:7000/collector/200',
 }
